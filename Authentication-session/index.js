50d6709d6a80aec5234d5ad88687ddd08f42b930
Hitesh <choudharyhitesh905@gmail.com>
2025-11-02 19:12:28 +0530

    intialised database connection with drizzle ORM

--- Authentication-session/index.js
+++ Authentication-session/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 const app  = express()
-const PORT = 8000
+const PORT = process.env.PORT??8000;
 
 app.use(express.json())//in built middleware
 
+app.get('/',(req,res)=>{
+    return res.status(200).json({message:`you are on homepage`})
+});
 
 app.listen(PORT,()=>{
     console.log(`Your server is running on PORT: ${PORT}`);
     
-})
+});
