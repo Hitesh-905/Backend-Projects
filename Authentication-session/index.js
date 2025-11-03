d51ee7181e8f6ad30da46d6ffbfacd618a3305af
Hitesh <choudharyhitesh905@gmail.com>
2025-11-03 20:42:24 +0530

    code before trying the try catch block recommended by gemini

--- Authentication-session/index.js
+++ Authentication-session/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
+import userRouter from './routes/users.routes.js'
 const app  = express()
-const PORT = process.env.PORT??8000;
+const PORT = process.env.PORT??3000;
 
 app.use(express.json())//in built middleware
 
 app.get('/',(req,res)=>{
     return res.status(200).json({message:`you are on homepage`})
 });
+
+app.use('/user', userRouter)
 
 app.listen(PORT,()=>{
     console.log(`Your server is running on PORT: ${PORT}`);
