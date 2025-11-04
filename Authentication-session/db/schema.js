d05f7e69294e7375e693cf422b4440f0da5c019e
Hitesh <choudharyhitesh905@gmail.com>
2025-11-04 11:28:29 +0530

    changes in the userstable in schema file

--- Authentication-session/db/schema.js
+++ Authentication-session/db/schema.js
@@ -1,20 +1,14 @@
-import { pgTable, varchar, text, uuid } from "drizzle-orm/pg-core";
 
+import {pgTable, varchar,text,uuid } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  password: text("password").notNull(),
-  salt: text("salt").notNull(),
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
 });
 
+// export const userSession = pgTable('users')
 
 
-// import {pgTable, varchar,text,uuid } from "drizzle-orm/pg-core";
-// export const usersTable = pgTable("users", {
-//   id: uuid().primaryKey().defaultRandom(),
-//   name: varchar({ length: 255 }).notNull(),
-//   email: varchar({ length: 255 }).notNull().unique(),
-//   password: text().notNull(),
-//   salt: text().notNull(),
-// });
+
