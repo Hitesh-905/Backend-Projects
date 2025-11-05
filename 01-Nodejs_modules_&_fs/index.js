ea137d78a423cc22f47eed7920f7b1b04028f11a
Hitesh <choudharyhitesh905@gmail.com>
2025-11-05 20:54:29 +0530

    more features of file system module of node

--- 01-Nodejs_modules_&_fs/index.js
+++ 01-Nodejs_modules_&_fs/index.js
@@ -2,4 +2,14 @@ const fs = require('node:fs')
 
 const content = fs.readFileSync('notes.txt','utf-8')
 
-console.log(content);
+// fs.writeFileSync('copy.txt',content,'utf-8')//this will over-ride the contents 
+fs.appendFileSync('copy.txt',content,'utf-8')//this will keep on appending the content 
+
+// fs.mkdirSync('games')
+
+
+fs.mkdirSync('games/valorant/skins',{recursive:true})//this will be creating multiple folders
+
+// fs.rmdirSync('games/valorant/skins');
+
+fs.rmdirSync('games/valorant');
