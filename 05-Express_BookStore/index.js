15c9f5437cf263859a45366b591a8c6b2fbea853
Hitesh <choudharyhitesh905@gmail.com>
2025-11-07 19:16:59 +0530

    succesful implementation of logger middleware to track the method and path request data

--- 05-Express_BookStore/index.js
+++ 05-Express_BookStore/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express()
+const fs = require('node:fs')
 const PORT = 8000;
 
+function loggerMiddleware (req,res,next){
+    const log = `\n[${Date.now()}] [${req.method}] [${req.path}]`;
+    fs.appendFileSync('logs.txt',log,'utf-8')
+    next()
+}
 app.use(express.json())//middleware to parse the request body into json format
+app.use(loggerMiddleware);
 //In memory database
 const books = [
   { id: 1, title: 'Book One', author: 'Author One' },
@@ -45,6 +52,16 @@ app.post('/books',(req,res)=>{
     return res.status(201).json({message:'Your book has been created successfully'})
 });
 
+app.delete('/books/:id',(req,res)=>{
+     const id = parseInt(req.params.id)
+      if(isNaN(id)){
+        return res.status(400).json({error:`Please Input a id with number `})
+    }
+    const indexToDelete = books.findIndex((eq)=>eq.id ===id)
+    books.splice(indexToDelete,1);
+
+    return res.status(201).json({messsge: 'Book successfully deleted'})
+})
 
 
 app.listen(PORT,()=>{
