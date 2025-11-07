c68d0caf1500c43d7106ccefa6417fd4d55f7fcd
Hitesh <choudharyhitesh905@gmail.com>
2025-11-07 15:49:24 +0530

    routes logic code before testing

---
+++ 05-Express_BookStore/index.js
@@ -0,0 +1,40 @@
+const { log } = require('node:console');
+const express = require('node:express');
+const { loadavg } = require('node:os');
+const app = express()
+const PORT = 8000;
+
+
+//In memory database
+const books = [
+  { id: 1, title: 'Book One', author: 'Author One' },
+  { id: 2, title: 'Book Two', author: 'Author Two' },
+];
+
+app.get('/',(req,res)=>{
+    res.end(`Welcome to My Book Store :)`)
+});
+
+app.get('/books',(req,res)=>{
+    res.json(books);
+});
+
+app.get('/books:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const book = books.find((e)=>e.id===id)
+
+    if(isNaN(id)){
+        return res.staus(400).json({error:`Please Input a id with number `})
+    }
+    if(!book){
+        return res.staus(404).json({error:`The book with following id:${id} is not found`})
+    }
+
+
+    return res.json(book);
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`You are listening on PORT:8000`);
+})
