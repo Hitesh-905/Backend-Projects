5cd9712e188fd53b07f0bbf2ac8232f3506264bb
Hitesh <choudharyhitesh905@gmail.com>
2025-11-07 19:37:52 +0530

    created route and middlewares folder to organize code by functionality

--- 05-Express_BookStore/index.js
+++ 05-Express_BookStore/index.js
@@ -1,69 +1,19 @@
 const express = require('express');
 const app = express()
-const fs = require('node:fs')
+const {loggerMiddleware} = require('./middlewares/logger.js')
+const bookRouter = require('./routes/books.routes.js')
 const PORT = 8000;
 
-function loggerMiddleware (req,res,next){
-    const log = `\n[${Date.now()}] [${req.method}] [${req.path}]`;
-    fs.appendFileSync('logs.txt',log,'utf-8')
-    next()
-}
-app.use(express.json())//middleware to parse the request body into json format
+//middleware to parse the request body into json format
+app.use(express.json())
+
+//custom middleware
 app.use(loggerMiddleware);
-//In memory database
-const books = [
-  { id: 1, title: 'Book One', author: 'Author One' },
-  { id: 2, title: 'Book Two', author: 'Author Two' },
-];
 
-app.get('/',(req,res)=>{
-    res.end(`Welcome to My Book Store :)`)
-});
 
-app.get('/books',(req,res)=>{
-    res.json(books);
-});
-
-app.get('/books/:id',(req,res)=>{
-    const id = parseInt(req.params.id)
-    const book = books.find((e)=>e.id===id)
-
-    if(isNaN(id)){
-        return res.status(400).json({error:`Please Input a id with number `})
-    }
-    if(!book){
-        return res.status(404).json({error:`The book with following id:${id} is not found`})
-    }
-    return res.json(book);
-});
-
-app.post('/books',(req,res)=>{
-   const{title,author}= req.body
-   if(!title || title === ''){
-    return res.status(400).json({error:`Please input the title`})
-   }
-   if(!author || author === ''){
-    return res.status(400).json({error:`Please input the author`})
-   }
-    const id = books.length+1
-    const book = {id,title,author}
-    books.push(book);
-
-    return res.status(201).json({message:'Your book has been created successfully'})
-});
-
-app.delete('/books/:id',(req,res)=>{
-     const id = parseInt(req.params.id)
-      if(isNaN(id)){
-        return res.status(400).json({error:`Please Input a id with number `})
-    }
-    const indexToDelete = books.findIndex((eq)=>eq.id ===id)
-    books.splice(indexToDelete,1);
-
-    return res.status(201).json({messsge: 'Book successfully deleted'})
-})
-
+//routes
+app.use('/',bookRouter);
 
 app.listen(PORT,()=>{
     console.log(`You are listening on PORT:8000`);
-})
+});
