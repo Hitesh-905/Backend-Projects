af6055e719e942004ff7d82e08112885c4ac63bf
Hitesh <choudharyhitesh905@gmail.com>
2025-11-08 16:25:41 +0530

    sql query of select from and where operations

--- 07-DrizzleORM/index.js
+++ 07-DrizzleORM/index.js
@@ -8,7 +8,6 @@ dotenv.config()
 
 //query 2 get only the name and email from the userstable
 //sql - SELECT name,email FROM userstable
-
 async function practice(){
     const allUsers = await db.select().from(userTable)
     console.log(allUsers);
@@ -20,3 +19,13 @@ async function practice(){
 
 }
 practice();
+
+//CRUD PART 2
+/*
+query 1 - select the user whose has a email hitesh@example.com
+SELECT * FROM usersTable WHERE email = hitesh@example.com
+
+query 2 - select the user whos has name hitesh and email hitesh@example.com
+SELECT * FROM usersTable WHERE email = hitesh@example.com AND name = hitesh
+
+*/
