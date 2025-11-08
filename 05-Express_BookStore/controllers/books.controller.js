31ce3df0d54936062c0d96e6f55ad188a1753d27
Hitesh <choudharyhitesh905@gmail.com>
2025-11-08 22:24:07 +0530

    all books routes successfully tested by postman

--- 05-Express_BookStore/controllers/books.controller.js
+++ 05-Express_BookStore/controllers/books.controller.js
@@ -1,5 +1,6 @@
 const booksTable = require('../models/book.model.js')
 const db = require('../db');
+const {eq} = require('drizzle-orm')
 
 
 exports.homePaage = function(req,res){
@@ -10,41 +11,39 @@ exports.getAllBooks = async function(req,res){
      return res.json(books);
 }
 
-exports.getBookByID = function(req,res){
-     const id = parseInt(req.params.id)
-    const book = BOOKS.find((e)=>e.id===id)
+exports.getBookByID = async function(req,res){
+     const id = req.params.id
+    const [book] = await db.select().from(booksTable).where(eq(booksTable.id,id))
 
-    if(isNaN(id)){
-        return res.status(400).json({error:`Please Input a id with number `})
-    }
+    // const [book] = await db.select().from(booksTable).where((table)=>eq(table.id,id))
+   
     if(!book){
         return res.status(404).json({error:`The book with following id:${id} is not found`})
     }
     return res.json(book);
 };
 
-exports.createBook = (req,res)=>{
-       const{title,author}= req.body
+exports.createBook = async (req,res)=>{
+       const{title,description,authorId}= req.body
    if(!title || title === ''){
     return res.status(400).json({error:`Please input the title`})
    }
-   if(!author || author === ''){
-    return res.status(400).json({error:`Please input the author`})
-   }
-    const id = BOOKS.length+1
-    const book = {id,title,author}
-    BOOKS.push(book);
+   const [result] = await db.insert(booksTable)
+   .values({
+    title,
+    description,
+    authorId
+   }).returning({
+    id: booksTable.id
+   });
 
-    return res.status(201).json({message:'Your book has been created successfully'})
+    return res.status(201).json({message:'Your book has been created successfully',id: result.id})
 };
 
-exports.deleteBookByID = function(req,res){
-      const id = parseInt(req.params.id)
-      if(isNaN(id)){
-        return res.status(400).json({error:`Please Input a id with number `})
-    }
-    const indexToDelete = BOOKS.findIndex((eq)=>eq.id ===id)
-    BOOKS.splice(indexToDelete,1);
+exports.deleteBookByID = async function(req,res){
+      const id = req.params.id
+     const deleteBook = await db.delete(booksTable).where(eq(booksTable.id,id))
+     .returning()
 
     return res.status(201).json({messsge: 'Book successfully deleted'})
 };
