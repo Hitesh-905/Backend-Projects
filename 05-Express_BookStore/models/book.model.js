022558501a782f52700957d2026c8af834ac401a
Hitesh <choudharyhitesh905@gmail.com>
2025-11-09 14:02:30 +0530

    imported sql helper from drizzle orm

--- 05-Express_BookStore/models/book.model.js
+++ 05-Express_BookStore/models/book.model.js
@@ -1,5 +1,5 @@
 const { pgTable, varchar, text, uuid, index } = require("drizzle-orm/pg-core");
-/
+const { sql } = require("drizzle-orm"); // <--- 1. Import the 'sql' helper
 const authorsTable = require('./author.model.js');
 
 const booksTable = pgTable('books', {
@@ -8,10 +8,10 @@ const booksTable = pgTable('books', {
     description: text(),
     authorId: uuid().references(() => authorsTable.id).notNull()
 }, (table) => ({
-    
+    // 2. This is the corrected line using the sql helper
     searchIndexonTitle: index('title_index').using(
         'gin',
-        table.title.withOps('gin_trgm_ops') // <--- Add this
+        sql`${table.title} gin_trgm_ops` // <--- Use this syntax
     ),
 }));
 
