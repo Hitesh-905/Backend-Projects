70166fc5c9b1533b0866e20efdb30a06c8145f4d
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-11-15 20:36:21 +0530

    Update index.js

--- 09-MongoDB/index.js
+++ 09-MongoDB/index.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = 8000;
 connectMongoDB(process.env.MONGODB_URL).then(()=> console.log(`MongoDB connected`));
 app.use(express.json())
-
+function logged(req,res){
+ console.log("under maintenance")
+}
 app.listen(PORT,()=>{
     console.log(`You are listening on PORT:${PORT}`);
     
